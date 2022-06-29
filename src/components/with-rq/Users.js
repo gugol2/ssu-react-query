d2f24a2279abb30a4f7b1bc65f4c9f8353b2e0c9
JS <jsanchezcriado@gmail.com>
2022-06-29 13:13:33 +0200

    Refactor Users components so that it uses object deconstruction

--- src/components/with-rq/Users.js
+++ src/components/with-rq/Users.js
@@ -2,23 +2,24 @@ import React from 'react';
 import useUsers from './useUsers';
 
 function Users(props) {
-  const query = useUsers();
-  if (query.isLoading) {
+  const {isLoading, data, error, isError, refetch} = useUsers();
+  
+  if (isLoading) {
     return <div>Cargando usuarios</div>;
   }
 
-  if (query.isError) {
-    return <div>Error cargando usuarios: {query.error?.message}</div>;
+  if (isError) {
+    return <div>Error cargando usuarios: {error?.message}</div>;
   }
 
   return (
     <div>
       <ul>
-        {query.data.data.map((user) => (
+        {data.data.map((user) => (
           <li key={user.id}>{user.email}</li>
         ))}
       </ul>
-      <button onClick={query.refetch}>Refrescar</button>
+      <button onClick={refetch}>Refrescar</button>
     </div>
   );
 }
